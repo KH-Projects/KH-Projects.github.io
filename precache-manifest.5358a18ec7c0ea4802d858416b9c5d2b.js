787994673a42aac3f81b4c26f161d59489b5420d
Karthik H <112889960+KH-Projects@users.noreply.github.com>
2022-11-11 02:51:35 +0530

    innitial_page

---
+++ precache-manifest.5358a18ec7c0ea4802d858416b9c5d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe881fbed8e63d173a6",
+    "url": "/static/css/main.34672eff.chunk.css"
+  },
+  {
+    "revision": "9fe881fbed8e63d173a6",
+    "url": "/static/js/main.9fe881fb.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "df76274e0d7095abc424",
+    "url": "/static/js/2.df76274e.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "724537850620e488f82f76ca528cb782",
+    "url": "/index.html"
+  }
+];
